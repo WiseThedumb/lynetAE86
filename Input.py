f8e89cdbb0a1412d08e3ad1ba010a0e2ead593be
Nikolaj <nila5@student.eadania.dk>
2024-10-09 11:51:45 +0200

    test

--- Input.py
+++ Input.py
@@ -1,11 +1,18 @@
 from sshkeyboard import listen_keyboard
 
 
+def press(w):
+    print(f"'{w}' pressed")
+def press(s):
+    print(f"'{s}' pressed")
+def press(a):
+    print(f"'{a}' pressed")
+def press(d):
+    print(f"'{d}' pressed")
 
 
 
 
 
-
-While true: 
+while true:
 listen_keyboard(on_press = press)
