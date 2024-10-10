a85f7563d8ca6e749ec95275f51e53f1ff10e961
Zhenjian Jiang <60202@student.eadania.dk>
2024-10-10 13:32:56 +0200

    4,11

--- testkode.py
+++ testkode.py
@@ -32,12 +32,12 @@ def set_motor(motor, direction, speed):
     
     pwm[motor].ChangeDutyCycle(speed)
 
-set_motor('h_front', 'forward', 50) #højre forreste
-set_motor('v_b', 'forward', 50)
+set_motor('h_front', 'backward', 50) #højre forreste
+set_motor('v_b', 'backward', 50)
 set_motor('h_b', 'backward', 50)
 set_motor('v_f', 'backward', 50)
 
-time.sleep(20)
+time.sleep(10)
 
 
 set_motor('h_front', 'backward', 75)
