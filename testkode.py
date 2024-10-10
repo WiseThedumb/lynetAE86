4c531cd4f169d1671b3681678829ebfcdfa510b7
Zhenjian Jiang <60202@student.eadania.dk>
2024-10-10 12:41:52 +0200

    4.08

--- testkode.py
+++ testkode.py
@@ -35,7 +35,7 @@ def set_motor(motor, direction, speed):
 set_motor('h_front', 'forward', 50) #højre forreste
 set_motor('v_b', 'backward', 50)
 set_motor('h_b', 'forward', 50)
-set_motor('v_f', 'backward', 50)
+set_motor('v_f', 'forward', 50)
 
 time.sleep(20)
 
