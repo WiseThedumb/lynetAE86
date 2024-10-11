b47a4278bf4a0b75861b4e87785e565744925d8f
Nikolaj Stilling Laustsen <nila5@student.eadania.dk>
2024-10-11 12:27:10 +0200

    dfgdggd

--- controlcar.py
+++ controlcar.py
@@ -40,10 +40,10 @@ for motor, pins in motor_pins.items():
 
 # Motor control functions
 def forward():
-    GPIO.output(motor_pins['h_front']['dir'], GPIO.HIGH)
+    GPIO.output(motor_pins['h_front']['dir'], GPIO.LOW)
     GPIO.output(motor_pins['v_back']['dir'], GPIO.HIGH)
     GPIO.output(motor_pins['h_back']['dir'], GPIO.HIGH)
-    GPIO.output(motor_pins['v_front']['dir'], GPIO.HIGH)
+    GPIO.output(motor_pins['v_front']['dir'], GPIO.LOW)
     pwm['h_front'].ChangeDutyCycle(75)
     pwm['v_back'].ChangeDutyCycle(75)
     pwm['h_back'].ChangeDutyCycle(75)
