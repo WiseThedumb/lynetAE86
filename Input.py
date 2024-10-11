aae07ded253771ff8f67143f1c47b8ef7c15d013
Nikolaj Stilling Laustsen <nila5@student.eadania.dk>
2024-10-11 07:57:36 +0200

    tastatur kontrol

--- Input.py
+++ Input.py
@@ -1,18 +1,36 @@
 from sshkeyboard import listen_keyboard
+def Goforward():
+    print("going forward")
+
+def Gobackward():
+    print ("Going Backwards")
+
+def Goleft():
+    print ("Going Left")
+
+def Goright():
+    print ("Going Right")
+
+def press(key):
+    switch = {
+        'w': Goforward,
+        's': Gobackward,
+        'a': Goleft,
+        'd': Goright
+    }
+    if key in switch:
+        switch[key]()
+    elif key == 'esc':  # press esc to stop
+        return False
+
+listen_keyboard(on_press=press)
 
 
-def press(w):
-    print(f"'{w}' pressed")
-def press(s):
-    print(f"'{s}' pressed")
-def press(a):
-    print(f"'{a}' pressed")
-def press(d):
-    print(f"'{d}' pressed")
 
 
 
 
 
-while true:
-    listen_keyboard(on_press = press)
+
+#while True:
+#    listen_keyboard(on_press = press)
