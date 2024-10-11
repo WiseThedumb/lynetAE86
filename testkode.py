14330a733f4d43bbffa83d84484cf0fe4861dd70
Nikolaj Stilling Laustsen <nila5@student.eadania.dk>
2024-10-11 11:04:10 +0200

    s

--- testkode.py
+++ testkode.py
@@ -51,7 +51,7 @@ try:
         if left_sensor == 0 and right_sensor == 0:
             # 前进
             set_motor('left_front', 'forward', 15)
-            set_motor('right_front', 'forward', 15)
+            set_motor('right_front', 'backward', 15)
             set_motor('left_rear', 'forward', 15)
             set_motor('right_rear', 'forward', 15)
         elif left_sensor == 1 and right_sensor == 0:
